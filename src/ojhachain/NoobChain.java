ca706aae67640e3ee7f3ec5fab0639bc238e5d76
Prathmesh Ojha <prathmeshojha2307@gmail.com>
2025-03-11 15:38:39 +0530

    Update NoobChain.java

---
+++ src/ojhachain/NoobChain.java
@@ -0,0 +1,26 @@
+package ojhachain;
+
+import java.security.Security;
+
+public class NoobChain {
+
+    public static Wallet walletA;
+    public static Wallet walletB;
+
+    public static void main(String[] args) {
+        //Setup Bouncy castle as a Security Provider
+        Security.addProvider(new org.bouncycastle.jce.provider.BouncyCastleProvider());
+        //Create the new wallets
+        walletA = new Wallet();
+        walletB = new Wallet();
+        //Test public and private keys
+        System.out.println("Private and public keys:");
+        System.out.println(StringUtil.getStringFromKey(walletA.privateKey));
+        System.out.println(StringUtil.getStringFromKey(walletA.publicKey));
+        //Create a test transaction from WalletA to walletB
+        Transaction transaction = new Transaction(walletA.publicKey, walletB.publicKey, 5, null);
+        transaction.generateSignature(walletA.privateKey);
+        //Verify the signature works and verify it from the public key
+        System.out.println("Is signature verified: " + (transaction.verifySignature() ? "Yes" : "No"));
+    }
+}
